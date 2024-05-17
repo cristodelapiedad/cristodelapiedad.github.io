2221b8cd88a9daa71723922563370487f9e16e4a
cristodelapiedad <santafaustina1938@gmail.com>
2024-05-17 16:30:57 -0500

    Add files via upload

---
+++ josefina-vannini/scripts.js
@@ -0,0 +1,7 @@
+
+
+let heightVieport = window.innerHeight;
+let heightBrowser = window.outerHeight;
+let heightBody = document.body.offsetHeight;
+
+console.log(heightVieport,heightBrowser,heightBody);
